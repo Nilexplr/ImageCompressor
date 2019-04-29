bbf1f16b8692e65a1b9fa08637989ec3384437f4
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-29 08:46:54 +0200

    [ADD] - Simple getOpt with error recognition

--- src/Argument.hs
+++ src/Argument.hs
@@ -3,7 +3,12 @@ module Argument
     ) where
 
 import System.Environment(getArgs)
+import System.Console.GetOpt
+import Control.Monad
 
+{-
+Options data declaration
+-}
 data Options = Options
     { helper            :: Bool
     , version           :: Bool
@@ -12,6 +17,9 @@ data Options = Options
     , pathImage         :: String
     } deriving Show
 
+{-
+Return a default Options data
+-}
 startOption :: Options
 startOption = Options
     { helper            = False
@@ -21,8 +29,30 @@ startOption = Options
     , pathImage         = ""
     }
 
+{-
+    Reference for the getOpt function
+-}
+options :: [ OptDescr (Options -> Either String Options) ]
+options =
+    [ Option ['c'] ["check"] (NoArg (\opt -> Right opt { helper = True } )) "Check ok"
+    ]
+-- DEBUG: This is a temporary version of the argument array
+
+{-
+Main function of the argument handling
+-}
 handleArgument :: IO (Either [String] Options)
 handleArgument = do
     argv <- getArgs
-    -- TODO: Implement the getopt
-    return $ Right startOption
+    opt <- parseArguments argv
+    return opt
+
+{-
+Parse every argument and return either an error or
+the correct Options data
+-}
+parseArguments :: [String] -> IO (Either [String] Options)
+parseArguments argv = do
+    case getOpt Permute options argv of
+        ([], [], []) -> return $ Left ["no args"]
+        (_, _, err) -> return $ Left err
